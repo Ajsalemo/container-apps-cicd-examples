21516e90086856ecf51b8f0e752405698a509765
Anthony Salemo <ajssalemo@gmail.com>
2023-08-15 22:54:49 -0400

    testing a new build

--- controllers/homeController.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (_req, res, _next) => {
-  res.json({ message: "azure-webapps-linux-node-express-basic" });
-});
-
-module.exports = router;
